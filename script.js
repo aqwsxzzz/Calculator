ced3eae9bbd487ad635080792d3609cae200d9df
ShegoS <jbarretolarrosa@gmail.com>
2021-08-26 17:13:01 -0300

    Finished CSS

--- script.js
+++ script.js
@@ -26,4 +26,47 @@ let operate = function (operator, num1, num2) {
         return divide(num1, num2);
     }
 }
-console.log(divide(20, 4))
+
+let container = document.querySelector("#container");
+const screenContainer = document.createElement("div");
+const buttonsContainer = document.createElement("div");
+screenContainer.setAttribute("id", "screencontainer");
+buttonsContainer.setAttribute("id", "buttonscontainer");
+container.appendChild(screenContainer);
+container.appendChild(buttonsContainer);
+const screen = document.createElement("textarea")
+screen.setAttribute("id", "screen");
+screenContainer.appendChild(screen);
+let arr = ["C", "Del", "X", "7", "8", "9", "/", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "=" ];
+console.log(arr.length, arr[17])
+for (let i = 0; i <= 4; i++) {
+    let buttonLines = document.createElement("div");
+    buttonsContainer.appendChild(buttonLines);
+    buttonLines.setAttribute("id", "buttonLine" + [i]);
+    buttonLines.setAttribute("class", "buttonLines");
+    if (i > 0 && i < 4) {
+        for (let j = 3; j <= 6; j++ ) {
+            let button = document.createElement("button");
+            let idNumber = (4* ([i] - 1));
+            let idNumber2 = parseInt(idNumber) + parseInt([j]);
+            buttonLines. appendChild(button);
+            button.setAttribute("class", "button");
+            button.setAttribute("id", "button" + idNumber2);
+            button.textContent = arr[idNumber2];
+        };    
+    }else if (i == 0) { for (let k = 0; k < 3; k++){
+        let button = document.createElement("button");
+        buttonLines.appendChild(button);
+        button.setAttribute("class", "button");
+        button.setAttribute("id", "button" + [k]);
+        button.textContent = arr[k];
+    }
+    }else for (let l = 15; l < 18; l++) {
+        let button = document.createElement("button");
+        buttonLines.appendChild(button);
+        button.setAttribute("class", "button");
+        button.setAttribute("id", "button" + [l]);
+        button.textContent = arr[l];
+    }
+}
+
